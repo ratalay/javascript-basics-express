6fcf5e9f8b12364b200961a4432fd4193e5a9b56
ratalay <rahmiiatalay@gmail.com>
2021-03-07 22:49:40 +0000

    numberst started

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings');
+const { add } = require('./lib/numbers');
 
 const app = express();
 
@@ -15,16 +16,8 @@ app.get('/strings/lower/:string', (req, res) => {
   res.status(200).json({ result: lowercase(req.params.string) });
 });
 
-app.get('/strings/first_character/:string', (req, res) => {
+/* app.get('/strings/first_character/:string', (req, res) => {
   res.status(200).json({ result: lowercase(req.params.string) });
-});
-
-/* app.get('/strings/first-characters/:id', (req, res) => {
-  if (req.query.length === 'undefined') {
-    res.json({ result: firstCharacters(req.params.id, 0) });
-  } else {
-    res.json({ result: firstCharacters(req.params.id, req.query.length) });
-  }
 }); */
 
 app.get('/strings/first-characters/:id', (req, res) => {
@@ -35,9 +28,11 @@ app.get('/strings/first-characters/:id', (req, res) => {
   }
 });
 
-/* app.get('/strings/hello/:string', (req, res) => {
-  res.status(200).json({ result: uppercase(req.params.string) });
-}); */
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+  res.status(200).json({ result: add(a, b) });
+});
 
 /* app.get('/strings/hello/turtle', (req, res) => {
   res.status(200).json({ result: 'Hello, turtle!' });
